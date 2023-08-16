e0c869b6c574ea5efa36a16eb789e6951624de6e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-16 09:15:54 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "root#index"
+
+  resources :greetings, only: [:index], defaults: {format: 'json'} do
+    collection do
+      get :random_greeting
+    end
+  end
+
+  # get '/greeting', to: 'root#index'
+
+  # get '/random_greeting', to: 'greetings#random_greeting'
 end
